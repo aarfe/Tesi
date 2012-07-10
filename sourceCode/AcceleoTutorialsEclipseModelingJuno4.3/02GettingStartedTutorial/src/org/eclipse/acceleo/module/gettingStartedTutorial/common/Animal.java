dad05daa95ffd0c46db5edd13ada13d973c029f2
aarfe <aarfe2@gmail.com>
2012-07-10 09:03:45 +0200

    deleted some examples and made a cleaner one

---
+++ sourceCode/AcceleoTutorialsEclipseModelingJuno4.3/02GettingStartedTutorial/src/org/eclipse/acceleo/module/gettingStartedTutorial/common/Animal.java
@@ -0,0 +1,7 @@
+	package 
+	//Start of user code imports
+	//End of user code
+
+   public class Animal {
+ 
+   }
---
+++ sourceCode/AcceleoTutorialsEclipseModelingJuno4.3/02GettingStartedTutorial/src/org/eclipse/acceleo/module/gettingStartedTutorial/common/Person.java
@@ -0,0 +1,20 @@
+	package 
+	//Start of user code imports
+	//End of user code
+
+   public class Person {
+     private String personSurname;
+
+     private String personName;
+     public String getPersonSurname() {
+       return this.personSurname;
+     }
+
+     public String getPersonName() {
+       return this.personName;
+     }
+ 
+     public invalid getAge() {
+       // TODO should be implemented
+     }
+   }
